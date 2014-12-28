f7b263f4ecada79ae15ac8df59f4c1296c6e5123
Philip Liberato <philipliberato@gmail.com>
2014-12-28 00:04:06 -0500

    Shuffled around files and added str methods for location_entity models

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 Basic ReadMe for the project.
+
+TODO:
+====
+
+- Find a proper place to keep track of tasks
+- Create a base.html file for extension
--- global_development/location_entity/models.py
+++ global_development/location_entity/models.py
@@ -3,8 +3,19 @@ from django.db import models
 class Country(models.Model):
     name = models.CharField(max_length=30)
 
+    def __str__(self):
+        return self.name
+
+
 class Region(models.Model):
     name = models.CharField(max_length=30)
 
+    def __str__(self):
+        return self.name
+
+
 class City(models.Model):
     name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
