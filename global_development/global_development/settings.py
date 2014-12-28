799d6c6a8022207b310f3ec53fe969449caf0e19
Philip Liberato <philipliberato@gmail.com>
2014-12-28 17:12:57 -0500

    setup static directory and greatly improved welcome page

--- global_development/global_development/settings.py
+++ global_development/global_development/settings.py
@@ -89,3 +89,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+    'Global-Development/global_development/static/',
+
+)
