1c3099a548cf55774acff3513ea98781e0a030c5
Philip Liberato <philipliberato@gmail.com>
2014-12-28 17:25:03 -0500

    connected a dummy sign in page

--- global_development/global_development/urls.py
+++ global_development/global_development/urls.py
@@ -4,6 +4,6 @@ from django.contrib import admin
 urlpatterns = patterns('',
 
     url(r'^$', 'global_development.views.welcome_page', name='welcome_page'),
-
+    url(r'^signin/$', 'global_development.views.signin', name='signin'),
     url(r'^admin/', include(admin.site.urls)),
 )
--- global_development/global_development/views.py
+++ global_development/global_development/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render_to_response
 
 def welcome_page(request):
     return render_to_response('welcome_page.html')
+
+def signin(request):
+	return render_to_response('signin.html')
