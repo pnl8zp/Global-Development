accd83d33bc10751086b41d9636706ff7d505417
Philip Liberato <philipliberato@gmail.com>
2014-12-28 20:02:10 -0500

    halfway decent skeleton for the unauthenticated side of the site

--- global_development/global_development/urls.py
+++ global_development/global_development/urls.py
@@ -5,5 +5,7 @@ urlpatterns = patterns('',
 
     url(r'^$', 'global_development.views.welcome_page', name='welcome_page'),
     url(r'^signin/$', 'global_development.views.signin', name='signin'),
+    url(r'^about/$', 'global_development.views.about', name='about'),
+    url(r'^contact/$', 'global_development.views.contact', name='contact'),
     url(r'^admin/', include(admin.site.urls)),
 )
--- global_development/global_development/views.py
+++ global_development/global_development/views.py
@@ -4,5 +4,14 @@ from django.shortcuts import render_to_response
 def welcome_page(request):
     return render_to_response('welcome_page.html')
 
+
 def signin(request):
-	return render_to_response('signin.html')
+    return render_to_response('signin.html')
+
+
+def about(request):
+    return render_to_response('welcome_about.html')
+
+
+def contact(request):
+    return render_to_response('contact.html')
